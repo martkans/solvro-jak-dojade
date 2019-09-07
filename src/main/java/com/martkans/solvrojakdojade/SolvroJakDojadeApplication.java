839b301badbce331872132b7c1616437c4e7a094
Martin Kansik <martkans@outlook.com>
2019-09-07 19:12:47 +0200

    SJD#master:init-project

---
+++ src/main/java/com/martkans/solvrojakdojade/SolvroJakDojadeApplication.java
@@ -0,0 +1,13 @@
+package com.martkans.solvrojakdojade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvroJakDojadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvroJakDojadeApplication.class, args);
+    }
+
+}
